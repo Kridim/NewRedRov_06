7e8102d1c18fb6e0ed2b960a491a936df2a226c8
Dima Kr <dimalrivenya@mail.ru>
2023-04-12 12:27:45 +0300

    FirstClass - ver.2.0

--- FirstGiC/src/Main.java
+++ FirstGiC/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!");
+        System.out.println("Hello!");
+        System.out.println("Hello!!!!!!!!");
     }
+    public static void hello(){
+        System.out.println("Hello!!!!!!!!!!!!!!");
+    }
+
 }
